5da67921bc1f264c6f94cdce19bd9280188aa5d7
aman khan <mohdamankhan18@gmail.com>
2023-11-26 20:15:42 +0530

    up 3

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ var cors = require("cors");
 app.use(express.json());
 app.use(cors())
 app.get("/", (req, res) => {
-  res.send("movie recomendation 2");
+  res.send("movie recommendation 2");
 });
+
 app.post('/recommend', async (req, res) => {
-  console.log("got api call", req.body.movie)
+  console.log("got API call", req.body.movie)
   const pythonProcess = spawn('python', ['final.py', req.body.movie]);
   let output = '';
+  let errorOutput = '';  // Add this line
+
   pythonProcess.stdout.on('data', (data) => {
     output += data.toString();
   });
-console.log(output, "output")
+
+  pythonProcess.stderr.on('data', (data) => {
+    errorOutput += data.toString();  // Add this line
+  });
+
   pythonProcess.on('close', (code) => {
     if (code === 0) {
       res.json({ recommendations: JSON.parse(output) });
     } else {
+      console.error('Python script error:', errorOutput);  // Log the error output
       res.status(500).send('Error executing Python script');
     }
   });
