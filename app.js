c31ae4e7c8f97f50a996582d62acc1ec5f204716
aman khan <mohdamankhan18@gmail.com>
2023-11-26 19:42:33 +0530

    up 3

--- app.js
+++ app.js
@@ -5,17 +5,16 @@ var cors = require("cors");
 app.use(express.json());
 app.use(cors())
 app.get("/", (req, res) => {
-  res.send("movie recomendation 1");
+  res.send("movie recomendation 2");
 });
 app.post('/recommend', async (req, res) => {
   console.log("got api call", req.body.movie)
   const pythonProcess = spawn('python', ['final.py', req.body.movie]);
-  console.log(pythonProcess)
   let output = '';
   pythonProcess.stdout.on('data', (data) => {
     output += data.toString();
   });
-console.log(output)
+console.log(output, "output")
   pythonProcess.on('close', (code) => {
     if (code === 0) {
       res.json({ recommendations: JSON.parse(output) });
